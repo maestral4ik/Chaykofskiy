d4d575c2067cef23f167151483727309b8a2f99e
Markin Maxim <maestral4ik@mail.ru>
2017-05-28 04:20:03 +0300

    first day

--- gulpfile.js
+++ gulpfile.js
@@ -12,21 +12,21 @@ gulp.task('connect', function(){
 });
 
 gulp.task('html', function(){
-  gulp.src('.app/*.html')
+  gulp.src('./app/*.html')
   .pipe(connect.reload());
 });
 gulp.task('css', function(){
-  gulp.src('.app/css/*.css')
+  gulp.src('./app/css/*.css')
   .pipe(connect.reload());
 });
 gulp.task('js', function(){
-  gulp.src('.app/js/*.js')
+  gulp.src('./app/js/*.js')
   .pipe(connect.reload());
 });
 gulp.task('watch', function() {
-  gulp.watch(['.app/*.html'], ['html']);
-  gulp.watch(['.app/css/*.css'], ['css']);
-  gulp.watch(['.app/js/*.js'], ['js']);
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/css/*.css'], ['css']);
+  gulp.watch(['./app/js/*.js'], ['js']);
 });
 
 gulp.task('default', ['connect', 'watch']);
